5d288fca5b26482c51cd7ba5a17d6502532af0ae
Shivam Suri <t-shivam.suri@ocltp.com>
2022-04-12 13:44:04 +0530

    resolving merge issues

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 const ConfigDB1 = require('./config/configDB1').configure
 const ConfigDB2 = require('./config/configDB2').configure
-const db1 = require('./query').connection1;
-const db2 = require('./query').connection2;
+
 
 console.log(process.argv)
 
-if(process.argv[2]){
+if(process.argv[2] && process.argv[3]){
     ConfigDB1(JSON.parse(process.argv[2]));
-    db1();
+    ConfigDB2(JSON.parse(process.argv[3]));
 }
 else{
-    console.error("No configurations found for DB1. Please Enter Database Configuration")
+    console.error("No configurations found for Databases. Please Enter Database Configurations")
 }
 
-if(process.argv[3]){
-    ConfigDB2(JSON.parse(process.argv[3]));
-    db2();
-}
-else{
-    console.error("No configurations found for DB2. Please Enter Database Configuration")
-}
+const db1 = require('./query');
 
--- query.js
+++ query.js
@@ -4,8 +4,10 @@
 let config1 = require('./config/configDB1').Config
 let config2 = require('./config/configDB2').Config
 var mysql = require('mysql2');
-// make connection to both dbs
+const inputFolder = './input';
+const fs = require('fs');
 
+// make connection to both dbs
 let con1,con2;
 function connection1(){
     console.log('In query',config1);
@@ -24,7 +26,7 @@ function connection2(){
     password: config2.password,
     database: config2.database
 });
-hitquery();
+//hitquery();
 }
 
 function hitquery(){
@@ -46,4 +48,30 @@ function hitquery(){
     });
 }
 
-module.exports = {connection1,connection2};
+// pick up queries from input folder
+function readInputFolder(){
+    fs.readdir(inputFolder, (err, files) => {
+        if(err){
+            console.log('Error Reading Input folder');
+        }
+    files.forEach(file => {
+        //Read query from the file and log
+        console.log(file);
+        fs.readFile(process.cwd()+'//input//'+file, 'utf8', function(err, data) {
+            if (err)
+            console.log(err);
+            const query = data;
+            console.log(query);
+          });
+      });
+    });
+}
+
+module.exports = {connection1,connection2}; //conn2 will be replaced by output file that we will write
+
+(function() {
+    connection1();
+    connection2();
+    readInputFolder();
+    hitquery();
+  })();
