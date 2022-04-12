caa08fd033f640bc506af6bd79c6c21d233728e3
Shivam Suri <t-shivam.suri@ocltp.com>
2022-04-12 10:58:31 +0530

    Added DB connections

--- query.js
+++ query.js
@@ -4,39 +4,49 @@ const config = require('./config/configDB2').Config
 const inputFolder = './input';
 const fs = require('fs');
 
+let config1 = require('./database1/config').Config
+let config2 = require('./database2/config').Config
+var mysql = require('mysql2');
 // make connection to both dbs
 
-function conn1(){
-    console.log('Connection here to db 1');
+let con1,con2;
+function connection1(){
+    console.log('In query',config1);
+    con1 = mysql.createConnection({
+    host: config1.host,
+    user: config1.user,
+    password: config1.password,
+    database: config1.database
+  });
 }
-
-function conn2(){
-    console.log('Connection here to db 2')
+function connection2(){
+    console.log('In query',config2);
+    con2 = mysql.createConnection({
+    host: config2.host,
+    user: config2.user,
+    password: config2.password,
+    database: config2.database
+});
+hitquery();
 }
-
-// pick up queries from input folder
-function readInputFolder(){
-    fs.readdir(inputFolder, (err, files) => {
-        if(err){
-            console.log('Error Reading Input folder');
-        }
-    files.forEach(file => {
-        //Read query from the file and log
-        console.log(file);
-        fs.readFile(process.cwd()+'//input//'+file, 'utf8', function(err, data) {
-            if (err)
-            console.log(err);
-            const query = data;
-            console.log(query);
-          });
-      });
+// node app.js '{"host":"localhost","user":"shivam","password":"qwerty777","database":"oldDatabase"}' '{"host":"localhost","user":"shivam","password":"qwerty777","database":"newDatabase"}'
+function hitquery(){
+    con1.connect(function(err) {
+        if (err) throw err;
+        con1.query("SELECT * FROM employee", function (err, result, fields) {
+          if (err) throw err;
+          console.log('From DB1')
+          console.log(result);
+        });
+    });
+    con2.connect(function(err) {
+        if (err) throw err;
+        con2.query("SELECT * FROM employee", function (err, result, fields) {
+          if (err) throw err;
+          console.log('From DB2')
+          console.log(result);
+        });
     });
 }
 
-module.exports = conn2; //conn2 will be replaced by output file that we will write
-
-(function() {
-    conn1();
-    conn2();
-    readInputFolder();
-  })();
+module.exports = {connection1,connection2};
