c8f865658b3c8836641b078c92164058f6fd34e0
Kartik Vasudev <t-kartik.vasudev@ocltp.com>
2022-04-12 10:40:39 +0530

    Added functionality for reading input folder and done some relevant changes

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
-const ConfigDB1 = require('./database1/config')
-const ConfigDB2 = require('./database2/config').configure
-const db2 = require('./query');
+const ConfigDB1 = require('./config/configDB1')
+const ConfigDB2 = require('./config/configDB2').configure
 //console.log("Hello")
 
 console.log(process.argv)
 
-//const db1 = JSON.parse(process.argv[2])
-//const db2 = process.argv[3]
-
-// console.log(db1)
-// console.log(db1.name)
-// console.log(db1.age)
-// console.log(db2)
-// console.log(typeof(db2))
-// console.log(db2.name)
-
-if(process.argv[2]){
+if(process.argv[2] && process.argv[3]){
     ConfigDB1(JSON.parse(process.argv[2]));
-}
-else{
-    console.error("No configurations found for DB1. Please Enter Database Configuration")
-}
-
-if(process.argv[3]){
     ConfigDB2(JSON.parse(process.argv[3]));
-    console.log('here',db2)
 }
 else{
-    console.error("No configurations found for DB2. Please Enter Database Configuration")
+    console.error("No configurations found for Databases. Please Enter Database Configurations")
 }
 
+const db2 = require('./query')
